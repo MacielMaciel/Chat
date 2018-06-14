81db2a8b94f079531f1ea572ca659bc6cf8751e7
MacielMaciel <MacielMaciel@users.noreply.github.com>
2018-06-14 08:34:10 -0300

    Servidor com modificações

--- servidor/index.js
+++ servidor/index.js
@@ -1,8 +1,12 @@
-
+// Import the ws module as a variable called WebSocketServer.
 var WebSocketServer = require("ws").Server;
+
+
 var wss = new WebSocketServer({port: 7007});
 
+
 console.log("Server is Running...");
+
 
 wss.broadcast = function broadcastMsg(msg) {
     wss.clients.forEach(function each(client) {
@@ -10,10 +14,13 @@ wss.broadcast = function broadcastMsg(msg) {
     });
 };
 
+
 wss.on('connection', function connection(ws) {
 
-
     var remoteIp = ws.upgradeReq.connection.remoteAddress;
+
+ 
     console.log('Connection received: ', remoteIp);
+
     ws.on('message', wss.broadcast);
 });
